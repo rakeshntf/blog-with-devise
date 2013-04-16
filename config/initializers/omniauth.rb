c6b47b3a5574f91d74a2bf90928cc208c144d2b0
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-16 12:09:58 +0530

    use-twiiter-popup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'BUE2EqG8smNV36Jh7Spvag', 'dIVz6CEvQvnMElzAjb2rt2bUPbhsproU24o22Vc'
-  
+  :display => "popup"
 end
