b781c1e0aecfea93a25905a595b490b7b1614cc8
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-17 20:05:06 +0530

    app/models/home.rb

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,12 +1,16 @@
 class Home < ActiveRecord::Base
   attr_accessible :name, :title,:avatar,:avatar_file_name,  :avatar_updated_at
 
- 
+ validates :name , :title , :presence => true
+validates_format_of :name, :title, :with => /^[^0-9`!@#\$%\^&*+_=]+$/
+
+
   has_attached_file :avatar, :styles => { :medium => "300x300>", 
   	:thumb => "100x100>" }, 
   :default_url => "/images/:style/missing.png"
-
-has_many :comments
+  validates :avatar, :presence => true
+	
+	has_many :comments
 
 
 end
