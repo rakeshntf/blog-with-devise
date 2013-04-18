14f73b0a53e73c82e12673b96b4dbb45d62f5a16
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-18 16:13:40 +0530

    config/initializers/omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+	 provider :facebook, '165600500269357', 'acd7243e174880a58ccb0351c8ac5461'
   provider :twitter, 'BUE2EqG8smNV36Jh7Spvag', 'dIVz6CEvQvnMElzAjb2rt2bUPbhsproU24o22Vc'
-  
+ 
 end
