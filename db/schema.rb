74a1cc43c9a16a7f48af5e2a36eb158433f38bf5
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-20 13:59:14 +0530

    db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419074741) do
+ActiveRecord::Schema.define(:version => 20130420064027) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -50,8 +50,9 @@ ActiveRecord::Schema.define(:version => 20130419074741) do
   create_table "line_items", :force => true do |t|
     t.integer  "home_id"
     t.integer  "cart_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "users", :force => true do |t|
