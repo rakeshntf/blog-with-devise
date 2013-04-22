1b0026dcbb74b11d92b3f4e404e11565408607f6
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-22 17:44:34 +0530

    all

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+
 end
 
 module Web
@@ -14,6 +15,7 @@ module Web
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+config.assets.paths << "#{Rails.root}/app/assets/videos"
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
