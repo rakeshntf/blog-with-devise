230ff855e13e57af450a67d195f3cc38799eee95
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-23 16:53:15 +0530

    app/models/video.rb

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,5 @@
 class Video < ActiveRecord::Base
   # attr_accessible :title, :body
-  has_attachment :content_type => :video, 
-                 :storage => :file_system, 
-                 :max_size => 300.megabytes
-
-  #turn off attachment_fu's auto file renaming 
-  #when you change the value of the filename field
-
+  
 
 end
