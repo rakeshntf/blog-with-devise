330219d96e9de94b4d461328165e7962260c6ded
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-26 19:07:36 +0530

    app/models/home.rb

--- app/models/home.rb
+++ app/models/home.rb
@@ -3,7 +3,7 @@ class Home < ActiveRecord::Base
 	  attr_accessible :name, :title,:avatar,:avatar_file_name,  :avatar_updated_at ,:video
 		 validates :name , :title , :presence => true
 			validates_format_of :name, :title, :with => /^[^0-9`!@#\$%\^&*+_=]+$/
-			  has_attached_file :avatar,	 :styles => { :medium => "300x300>", 
+			  has_attached_file :avatar, 	 :styles => { :medium => "300x300>", 
 			  :thumb => "100x100>" }
 			 
 			  
