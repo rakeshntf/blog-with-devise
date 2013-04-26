f19d2391b65f58793840228d606ff812d2048ae0
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-26 14:53:58 +0530

    add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -134,7 +134,7 @@ var dropbox = document.getElementById("dropbox")
 dropbox.addEventListener("dragenter", dragEnter, false);
 dropbox.addEventListener("dragexit", dragExit, false);
 dropbox.addEventListener("dragover", dragOver, false);
-dropbox.addEventListener("drop", drop, false);
+dropbox.addEventListener("drop", drop, true);
 
 
 function dragEnter(evt) {
